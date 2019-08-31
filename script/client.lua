068cc91549b851aad8613940732d596ac4edcb87
Marco <marco.paggioro@gmail.com>
2019-08-31 14:01:07 +0200

    Update client.lua

---
+++ script/client.lua
@@ -0,0 +1,54 @@
+local ESX = nil
+local isTalking = false
+
+CreateThread(function()
+	while ESX == nil do
+		TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+	ESX.UI.HUD.SetDisplay(0.0)
+end)
+
+--Starter state
+RegisterNetEvent("esx:playerLoaded")
+AddEventHandler("esx:playerLoaded", function(xPlayer)
+	--Money
+	SendNUIMessage({action = "setValue", key = "money", value = xPlayer.money .. " €"})
+	
+	--Job
+	local job = xPlayer.job
+	SendNUIMessage({action = "setValue", key = "job", value = job.label .. " - " .. job.grade_label})
+end)
+
+--Voice
+CreateThread(function()
+	while true do
+		Wait(0)
+		if isTalking == false then
+			if NetworkIsPlayerTalking(PlayerId()) then
+				isTalking = true
+				SendNUIMessage({action = "setTalking", value = true})
+			end
+			else if NetworkIsPlayerTalking(PlayerId()) == false then
+				isTalking = false
+				SendNUIMessage({action = "setTalking", value = false})
+			end
+		end
+	end
+end)
+
+--Update state
+RegisterNetEvent("es:activateMoney")
+AddEventHandler("es:activateMoney", function(e)
+	SendNUIMessage({action = "setValue", key = "cash", value = ESX.Math.GroupDigits(e) .. " €"})
+end)
+
+RegisterNetEvent("esx:setJob")
+AddEventHandler("esx:setJob", function(job)
+  SendNUIMessage({action = "setValue", key = "job", value = job.label .. " - " .. job.grade_label})
+end)
+
+RegisterNetEvent("mine_customui:updateStatus")
+AddEventHandler("mine_customui:updateStatus", function(status)
+	SendNUIMessage({action = "updateStatus", status = status})
+end)
