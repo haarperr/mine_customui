01ba2614226763dff096346e8ef1880ffe675ed5
Marco <marco.paggioro@gmail.com>
2019-09-01 18:45:04 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
-description "Custom UI by Marco Paggioro -> https://www.marcopaggioro.it/ & Mirko"
+description "Custom UI by Marco Paggioro -> https://www.marcopaggioro.it/"
 version "1.0"
 
 client_script "script/client.lua"
