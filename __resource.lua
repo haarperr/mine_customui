461dd1317352f1a27c30682575b262568dfc0150
ItalianParadise <54775287+ItalianParadise@users.noreply.github.com>
2019-09-01 19:00:57 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,7 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 description "Custom UI by Marco Paggioro -> https://www.marcopaggioro.it/ & Mirko"
 version "1.0"
+version_ciao "2.0"
 
 client_script "script/client.lua"
 
